128f964afc47f0e19aa7e0a45f6ca2cb41e73be3
Notim <notim@noreply.com>
2018-08-17 22:22:17 -0300

    aplicando loop no app

--- agenda.py
+++ agenda.py
@@ -1,19 +1,17 @@
 #Agenda Telefonica
 import funcoes
+while 1:
+	funcoes.bemvindo()
 
-funcoes.bemvindo()
-#Opcoes do Usuario
-opcao = int(input())
-
-#Estrutura de controle
-if opcao == 1:
-	funcoes.adicionar()
-elif opcao == 2:
-	funcoes.listar()
-elif opcao == 3:
-	funcoes.buscaNome(input("Digite o contato"))
-elif opcao == 4:
-	funcoes.delete(input("Digite o contato"))
-else:
-	funcoes.sair()
+	opcao = int(input())
+	if opcao == 1:
+		funcoes.adicionar()
+	elif opcao == 2:
+		funcoes.listar()
+	elif opcao == 3:
+		funcoes.buscaNome(input("Digite o contato: "))
+	elif opcao == 4:
+		funcoes.delete(input("Digite o contato: "))
+	else:
+		funcoes.sair()
 
